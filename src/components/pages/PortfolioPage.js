59e90ff75179c7ef94921bf46054b42b5bdae6a9
Sebastian <smcasanova17@hotmail.com>
2023-02-28 20:12:58 -0500

    fixed push issue

---
+++ src/components/pages/PortfolioPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../../logo.svg';
+
+const PortfolioPage = () =>
+{
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default PortfolioPage;
+
