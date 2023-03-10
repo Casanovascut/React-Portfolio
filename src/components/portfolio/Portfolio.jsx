109203eff8d07cc073bdfeb42b9aac8c8ab8b99e
Sebastian <smcasanova17@hotmail.com>
2023-03-09 21:42:28 -0500

    fixed image issue

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import './portfolio.css'
-
+import knickknack from '../../assets/knickknack.png'
 
 const Portfolio = () =>
 {
     return (
+        <div>
         <a href='https://sheltered-ridge-71744.herokuapp.com/'>
-            <img src='../../assets/knickknack.png' />
+            <img src={knickknack} />
         </a>
+        <a href='https://sheltered-ridge-71744.herokuapp.com/'>
+            <img src={knickknack} />
+        </a>
+        <a href='https://sheltered-ridge-71744.herokuapp.com/'>
+            <img src={knickknack} />
+        </a>
+        <a href='https://sheltered-ridge-71744.herokuapp.com/'>
+            <img src={knickknack} />
+        </a>
+        </div>
     )
 }
 
